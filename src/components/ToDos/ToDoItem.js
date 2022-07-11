45273b7b98994d63227f117574b0479979a5787f
NoobSaibot <kra4kas@gmail.com>
2022-07-11 19:10:27 +0200

    add change complited

--- src/components/ToDos/ToDoItem.js
+++ src/components/ToDos/ToDoItem.js
@@ -1,28 +1,38 @@
 import { Box, Button, Switch } from '@mui/material'
-import React from 'react'
+import React, {useState} from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
 import axios from 'axios';
+import './todoStyle.scss'
 
 
 export default function ToDoItem(props) {
 
-    const deleteClick = (id)=>{
-        axios.delete('https://noobsaibot-todo.herokuapp.com/api/del', {id: id})
-        .then(console.log(id))
+  const [checked, setChecked] = useState(props.checked)
+  
+
+    const deleteClick = ()=>{
+        axios.delete('https://noobsaibot-todo.herokuapp.com/api/del', {id: props.delId})
+        .then(console.log(props.delId))
+      }
+
+      const complitedClick =()=>{
+        setChecked(!checked)
+        axios.put('https://noobsaibot-todo.herokuapp.com/api/put', {id: props.delId, complited: !checked})
+        .then(console.log(props.delId))
       }
     
     return (
-      <Box style={{display:'flex', flexDiraction:'row', alignContent:'center', alignItems:'center', widht:'100%', justifyContent:'space-evenly'}}>
+      <Box className='todo'>
           <Switch
-            checked={props.checked}
-            onChange={props.handleChange}
+            checked={checked}
+            onChange={complitedClick}
             inputProps={{ 'aria-label': 'controlled' }}
         />
-        <h1>{props.title}</h1>
+        <h1 className={checked?"todo_title":null}>{props.title}</h1>
         <Button
          variant="text" 
          color='error'
-         onClick={()=>deleteClick(props.delId)}>
+         onClick={deleteClick}>
              <DeleteIcon/>
         </Button>
       </Box>
